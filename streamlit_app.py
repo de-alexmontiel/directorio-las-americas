477668e61309c3f0df9064e420cadf05ff37f02a
de-alexmontiel <160831123+de-alexmontiel@users.noreply.github.com>
2024-03-30 20:03:47 +0000

    Fase 1 terminada. Mapa con pop ups sin dato correcto

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,76 @@ def main():
 
     # Código HTML para incrustar el mapa de Mapbox
     html_code = """
-   <iframe width='100%' height='400px' src="https://api.mapbox.com/styles/v1/talegaman5000/clu62pv7p00jq01pa350vddfh.html?title=false&access_token=pk.eyJ1IjoidGFsZWdhbWFuNTAwMCIsImEiOiJjbGo1YXM3ZHMwMm45M2dvNXVtaTZ4dmlpIn0.KwWuHIQzGYotPniqszMunQ&zoomwheel=false#15.14/21.067595/-89.648999" title="directorio americas" style="border:none;"></iframe>
-    """
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8' />
+    <title>Points on a map</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.js'></script>
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.css' rel='stylesheet' />
+    <style>
+      body { 
+        margin: 0; 
+        padding: 0; 
+      }
+
+      #map { 
+        position: absolute; 
+        top: 0; 
+        bottom: 0; 
+        width: 100%; 
+      }
+    </style>
+  </head>
+  <body>
+    <div id='map'></div>
+    <script>
+    // The value for 'accessToken' begins with 'pk...'
+    mapboxgl.accessToken = 'pk.eyJ1IjoidGFsZWdhbWFuNTAwMCIsImEiOiJjbGo1YXM3ZHMwMm45M2dvNXVtaTZ4dmlpIn0.KwWuHIQzGYotPniqszMunQ'; 
+    const map = new mapboxgl.Map({
+      container: 'map',
+      // Replace YOUR_STYLE_URL with your style URL.
+      style: 'mapbox://styles/talegaman5000/clu62pv7p00jq01pa350vddfh', 
+      center: [-89.65558693411182, 21.070582338532652],
+      zoom: 14
+    });
+
+    // Code from the next step will go here.
+    /* 
+Add an event listener that runs
+  when a user clicks on the map element.
+*/
+map.on('click', (event) => {
+  // If the user clicked on one of your markers, get its information.
+  const features = map.queryRenderedFeatures(event.point, {
+    layers: ['directorio'] // replace with your layer name
+  });
+  if (!features.length) {
+    return;
+  }
+  const feature = features[0];
+
+  // Code from the next step will go here.
+
+/* 
+    Create a popup, specify its options 
+    and properties, and add it to the map.
+  */
+  const popup = new mapboxgl.Popup({ offset: [0, -15] })
+  .setLngLat(feature.geometry.coordinates)
+  .setHTML(
+    `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+  )
+  .addTo(map);
+
+});
+
+    </script>
+  </body>
+</html>
+
+"""
     st.components.v1.html(html_code, width=700, height=600)
 
 if __name__ == "__main__":
